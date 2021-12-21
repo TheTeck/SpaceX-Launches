bac116fe83b15b734158d17d1f54ba4df9a971a1
John Teckemeyer <johnteckemeyer@gmail.com>
2021-12-20 21:39:20 -0800

    Change the color scheme to interpolate the end angle of each segment

--- src/components/CustomerPieChart/CustomerPieChart.jsx
+++ src/components/CustomerPieChart/CustomerPieChart.jsx
@@ -14,21 +14,13 @@ export default function CustomerPieChart ({ customers }) {
         const pieData = d3.pie().value(d => d.count)(customers);
 
         // Define arcs for graphing and labling
-        const arc = d3.arc().innerRadius(120).outerRadius(200);
-
-        const colors = d3.scaleOrdinal(d3.schemeTableau10);
+        const arc = d3.arc().innerRadius(120).outerRadius(200)
 
         const svg = d3.select(svgRef.current)
             .attr('width', 400)
             .attr('height', 400)
             .append('g')
                 .attr('transform', 'translate(200, 200)')
-
-        const toolDiv = d3.select('#piechart-area')
-            .append('div')
-            .style('visibility', 'hidden')
-            .style('position', 'absolute')
-            .style('background-color', 'red')
 
         const displayDiv = d3.select(displayRef.current)
         
@@ -37,18 +29,15 @@ export default function CustomerPieChart ({ customers }) {
             .data(pieData)
             .join('path')
                 .attr('d', arc)
-                .attr('fill', (d,i) => colors(i))
+                .attr('stroke', 'black')
+                .attr('fill', d => d3.interpolateInferno(d.endAngle / (2* Math.PI)))
                 .on('mouseover', (e,d) => {
                     displayDiv
                         .append('g')
                             .append('p')
                                 .text(d.data.customer)
-                                
                             .append('p')
-                                .text(d.data.count + (d.data.count === 1 ? ' Payload' : ' Payloads'))
-                                //.style('width', '100%')
-
-                                
+                                .text(d.data.count + (d.data.count === 1 ? ' Payload' : ' Payloads'))                                
                 })
                 .on('mouseout', () => {
                     displayDiv.select('g').remove();
