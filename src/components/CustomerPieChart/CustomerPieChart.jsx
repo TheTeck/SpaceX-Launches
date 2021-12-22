d528d9954be81cbf4c12f2a1c9062839cda9d40b
John Teckemeyer <johnteckemeyer@gmail.com>
2021-12-21 19:41:11 -0800

    Add title to piechart and make proper placement for chart to be seen better on smaller screens

--- src/components/CustomerPieChart/CustomerPieChart.jsx
+++ src/components/CustomerPieChart/CustomerPieChart.jsx
@@ -18,9 +18,9 @@ export default function CustomerPieChart ({ customers }) {
 
         const svg = d3.select(svgRef.current)
             .attr('width', 400)
-            .attr('height', 400)
+            .attr('height', 600)
             .append('g')
-                .attr('transform', 'translate(200, 200)')
+                .attr('transform', 'translate(200, 300)')
 
         const displayDiv = d3.select(displayRef.current)
         
@@ -42,10 +42,17 @@ export default function CustomerPieChart ({ customers }) {
                 .on('mouseout', () => {
                     displayDiv.select('g').remove();
                 })
-                
+        
+        svg
+            .append('g')
+                .append('text')
+                    .text('Customer Payloads')
+                    .attr('text-anchor', 'middle')
+                    .attr('transform', 'translate(0, -250)')
+                    .style('fill', 'white')
+                    .style('font-size', '1.5rem')
     })
 
-    console.log(customers)
     return (
         <div id="piechart-area">
             <svg ref={svgRef}></svg>
