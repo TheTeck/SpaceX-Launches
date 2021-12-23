21753396d5732a9c2bd03fbd231fcbada034a3c2
John Teckemeyer <johnteckemeyer@gmail.com>
2021-12-22 23:05:35 -0800

    Create the x and y axis for line graph with formatted ticks

--- src/components/LaunchFrequency/LaunchFrequency.jsx
+++ src/components/LaunchFrequency/LaunchFrequency.jsx
@@ -1,13 +1,70 @@
 import React, { useRef, useEffect } from 'react';
+import * as d3 from 'd3';
 
 import './LaunchFrequency.scss';
 
 export default function LaunchFrequency ({ launches }) {
 
     const svgRef = useRef();
+    const width = 600;
+    const height = 400;
+    const margin = { top: 50, right: 50, bottom: 50, left: 50};
+
+    const years = new Map();
+
+    launches.forEach(launch => {
+        const year = launch.launch_year;
+
+        if (years.has(year)) {
+            years.set(year, years.get(year) + 1);
+        } else {
+            years.set(year, 1);
+        }
+    });
+
+    const data = [];
+
+    for (const [key, value] of years) {
+        data.push({ year: +key, count: value })
+    }
+
+    const max = data.reduce((acc, year) => {
+        return year.count > acc ? year.count : acc;
+    }, 0);
+
+    const min = data.reduce((acc, year) => {
+        return year.count < acc ? year.count : acc
+    }, data[0].count)
 
     useEffect(() => {
+        const svg = d3.select(svgRef.current)
+            .attr('width', width)
+            .attr('height', height)
+            .style('background-color', 'yellow')
+    
+        const xScale = d3.scaleLinear()
+            .domain([0, data.length-1])
+            .range([margin.left, width - margin.right])
+        
+        const yScale = d3.scaleLinear()
+            .domain([min - 1, max])
+            .range([height - margin.bottom, margin.top])
+        
+        const xAxis = d3.axisBottom(xScale)
+            .ticks(data.length)
+            .tickFormat(i => +launches[0].launch_year + i)
 
+        const yAxis = d3.axisLeft(yScale)
+            .ticks(20)
+            .tickFormat(d => d)
+
+        svg.append('g')
+            .call(xAxis)
+            .attr('transform', `translate(0, ${height - margin.bottom })`)
+        
+        svg.append('g')
+            .call(yAxis)
+            .attr('transform', `translate(${margin.left}, 0)`)
     })
 
     return (
